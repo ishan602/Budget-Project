b373058d436d617439308e02e5800656db934a6b
Ishan Kumar <ishan.k@tadigital.com>
2020-03-03 13:29:33 +0530

    First Commit

---
+++ budget/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import Top from "./components/Top/Top";
+// import Bottom from "./components/Bottom/Bottom";
+// import Form from "./components/Form/Form";
+
+function App() {
+  return (
+    <>
+      {/* <Top></Top>
+      <Form></Form>
+      <Bottom></Bottom> */}
+      <h1>Hi</h1>
+    </>
+  );
+}
+
+export default App;
